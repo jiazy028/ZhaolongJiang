69dd197520fba880851c29d1ea52a5f9e9905e80
jiazy028 <129717360+jiazy028@users.noreply.github.com>
2024-03-27 18:33:00 +1030

    Add files via upload
    First commit for my project

---
+++ Assignment1_Deadly_corp/Assignment1_Deadly_corp.cpp
@@ -0,0 +1,12 @@
+﻿// Assignment1_Deadly_corp.cpp : Defines the entry point for the application.
+//
+
+#include "Assignment1_Deadly_corp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
